89a44f3ff6ef3d2d1969514fd2ed6eda9daee6c0
Sarahosnyss <https://github.com/Sarahosnyss/alx-low_level_programming.git>
2023-03-27 13:16:23 -0700

    first 4

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer to the string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
