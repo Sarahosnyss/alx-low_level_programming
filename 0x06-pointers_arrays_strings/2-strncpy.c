bfadf73e4c05cf8add6e47de3370b7354a41ddac
Sarahosnyss <https://github.com/Sarahosnyss/alx-low_level_programming.git>
2023-03-29 13:39:05 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source string
+ * @n: maximum number of bytes to be copied from src
+ *
+ * Return: pointer to the destination buffer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+	dest[i] = src[i];
+	i++;
+	}
+
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+
+	return (dest);
+}
