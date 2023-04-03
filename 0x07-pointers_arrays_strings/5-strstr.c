fe12f6e486c8b7393b06e8ad30d8b4e7674de99e
Sarahosnyss <https://github.com/Sarahosnyss/alx-low_level_programming.git>
2023-04-03 04:44:03 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: pointer to the string to be searched.
+ * @needle: pointer to the substring to be located
+ *
+ * Return: a pointer to the beginning of the located substring,
+ * NULL if the substrng is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
