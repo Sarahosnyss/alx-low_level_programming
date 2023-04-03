809c7f80e96c0331043149dc7c13b85baa5f4e93
Sarahosnyss <https://github.com/Sarahosnyss/alx-low_level_programming.git>
2023-04-03 04:15:25 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_memcpy - copies n bytes from memory area src to memory area
+ *@dest: memory where is stored
+ *@src:  pointer to the source memory area.
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[j];
+                j++;
+                n--;
+		i++;
+	}
+	return (dest);
+}
