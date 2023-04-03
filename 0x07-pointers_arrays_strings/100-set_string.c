b69cb4c1dc7654d710d92c09b110412c17c38472
Sarahosnyss <https://github.com/Sarahosnyss/alx-low_level_programming.git>
2023-04-03 13:15:29 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - locates a character in a string
+ * @s: a pointer of a pointer type char
+ * @to: a pointer type char
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
